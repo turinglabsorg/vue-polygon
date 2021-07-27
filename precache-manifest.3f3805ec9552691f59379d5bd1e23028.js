0ff77793c357c15ba0c7b2873b3db4ac2a76f322
turinglabsorg <s.cataudo@turinglabs.org>
2021-07-28 00:27:01 +0200

    ghpages

---
+++ precache-manifest.3f3805ec9552691f59379d5bd1e23028.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b97f23021e9ad9a924",
+    "url": "/css/app.fc2324d3.css"
+  },
+  {
+    "revision": "6ac5b891036ead0ad5a5",
+    "url": "/css/chunk-vendors.7ef2ddfa.css"
+  },
+  {
+    "revision": "5480961c77689b691922c74d7c00aebd",
+    "url": "/favicon.svg"
+  },
+  {
+    "revision": "4cdf43a0550cd1a5e09bba56dc39034c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08e2a5aa48aff782a73f",
+    "url": "/js/about.88f3f593.js"
+  },
+  {
+    "revision": "39b97f23021e9ad9a924",
+    "url": "/js/app.2b17dcc9.js"
+  },
+  {
+    "revision": "6ac5b891036ead0ad5a5",
+    "url": "/js/chunk-vendors.dd509f66.js"
+  },
+  {
+    "revision": "a9775f2deeb6277a94b017d65fb3997f",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "43cf2182105f25141781a328e9e4d71a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
