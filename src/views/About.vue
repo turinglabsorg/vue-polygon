81b55d9099b96862084b00877b6bf6482e3685f0
turinglabsorg <s.cataudo@turinglabs.org>
2021-07-28 00:25:54 +0200

    page

--- src/views/About.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="about">
-    <h1>VueJS - Polygon starter</h1>
-    Created in a July midnight by
-    <a href="https://github.com/nft-studio"><b>nft</b>studio</a>
-  </div>
-</template>
